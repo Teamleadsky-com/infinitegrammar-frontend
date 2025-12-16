b0c7a448d59bec2ccc1ae7d0e0461785b6469fe1
Aleksandr Zuravliov <alex@teamleadsky.com>
2025-12-16 10:49:22 +0100

    Scroll up for grammar pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import { AuthProvider } from "@/contexts/AuthContext";
+import { ScrollToTop } from "@/components/ScrollToTop";
 import LevelSelection from "./pages/LevelSelection";
 import Exercise from "./pages/Exercise";
 import Statistics from "./pages/Statistics";
@@ -30,6 +31,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
+            <ScrollToTop />
             <Routes>
               <Route path="/" element={<LevelSelection />} />
               <Route path="/exercise" element={<Exercise />} />
