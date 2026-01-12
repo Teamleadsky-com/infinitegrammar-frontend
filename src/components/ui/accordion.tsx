c5de5eae38b43d14dadd0b93ebab609bf1b0460d
Aleksandr Zuravliov <alex@teamleadsky.com>
2026-01-12 21:03:49 +0100

    FAQ bug fixed

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -41,7 +41,7 @@ const AccordionContent = React.forwardRef<
   <AccordionPrimitive.Content
     ref={ref}
     forceMount
-    className="overflow-hidden text-sm transition-all data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+    className="overflow-hidden text-sm transition-all data-[state=closed]:h-0 data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
     {...props}
   >
     <div className={cn("pb-4 pt-0", className)}>{children}</div>
